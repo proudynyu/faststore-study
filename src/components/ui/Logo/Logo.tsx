9ba4f900a41fb39886cad2cfa8f68b40c951f420
Marcos Ewbank <ewbankmarcos@gmail.com>
2022-06-23 10:22:42 -0300

    Initialize feat/theming-filter-fsss-380

---
+++ src/components/ui/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+
+function Logo() {
+  return (
+    <img
+      src="/logo.svg"
+      alt="The store logo with with a stylized letter B in a dark blue gradient background"
+      width="176px"
+      height="30px"
+    />
+  )
+}
+
+export default Logo
